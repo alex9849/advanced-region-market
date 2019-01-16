7bb2e895945679fc5c4860fe8edd85b650b213b4
alex9849 <alexander@liggesmeyer.de>
2019-01-16 20:03:41 +0100

    added allowedSubregions to config

--- advancedregionmarket/src/main/java/net/alex9849/arm/Handler/ARMListener.java
+++ advancedregionmarket/src/main/java/net/alex9849/arm/Handler/ARMListener.java
@@ -110,7 +110,7 @@ public class ARMListener implements Listener {
 
                 LinkedList<Sign> sellsign = new LinkedList<Sign>();
                 sellsign.add((Sign) sign.getBlock().getState());
-                SellRegion addRegion = new SellRegion(region, regionWorld, sellsign, price, false, autoReset, isHotel, doBlockReset, regionkind, null,1, true, new ArrayList<Region>(), 2);
+                SellRegion addRegion = new SellRegion(region, regionWorld, sellsign, price, false, autoReset, isHotel, doBlockReset, regionkind, null,1, true, new ArrayList<Region>(), 0);
                 addRegion.createSchematic();
                 net.alex9849.arm.regions.RegionManager.addRegion(addRegion);
                 sign.getPlayer().sendMessage(Messages.PREFIX + Messages.REGION_ADDED_TO_ARM);
@@ -210,7 +210,7 @@ public class ARMListener implements Listener {
                 sellsign.add((Sign) sign.getBlock().getState());
 
                 RentRegion addRegion = new RentRegion(region, regionWorld, sellsign, price, false, autoReset, isHotel, doBlockReset, regionkind, null,
-                        1, true,1, maxRentTime, extendPerClick, new ArrayList<Region>(), 2);
+                        1, true,1, maxRentTime, extendPerClick, new ArrayList<Region>(), 0);
                 addRegion.createSchematic();
                 net.alex9849.arm.regions.RegionManager.addRegion(addRegion);
 
@@ -304,7 +304,7 @@ public class ARMListener implements Listener {
                 sellsign.add((Sign) sign.getBlock().getState());
 
                 ContractRegion addRegion = new ContractRegion(region, regionWorld, sellsign, price, false, autoReset, isHotel, doBlockReset, regionkind, null,
-                        1, true, extendtime, 1, false, new ArrayList<Region>(), 2);
+                        1, true, extendtime, 1, false, new ArrayList<Region>(), 0);
                 addRegion.createSchematic();
                 net.alex9849.arm.regions.RegionManager.addRegion(addRegion);
                 sign.getPlayer().sendMessage(Messages.PREFIX + Messages.REGION_ADDED_TO_ARM);

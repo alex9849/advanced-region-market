f7d7567d648081ac1782f73898b37e709bbd64a2
alex9849 <alexander@liggesmeyer.de>
2019-04-23 14:46:45 +0200

    added some event classes

--- advancedregionmarket/src/main/java/net/alex9849/arm/events/AddRegionEvent.java
+++ advancedregionmarket/src/main/java/net/alex9849/arm/events/AddRegionEvent.java
@@ -3,7 +3,7 @@ package net.alex9849.arm.events;
 import net.alex9849.arm.regions.Region;
 
 /**
- * Is called if a region is added to the RegionManager
+ * Is called if a region gets added to the RegionManager
  */
 public class AddRegionEvent extends RegionEvent{
     public AddRegionEvent(Region region) {

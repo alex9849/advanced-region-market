a4d9b673478c29a708b05ac28f703c3f42095cce
alex9849 <alexander@liggesmeyer.de>
2019-10-04 15:50:26 +0200

    Added a funktion that should trick luckperms "unsafe vault operation" blocker

--- advancedregionmarket/src/main/java/net/alex9849/arm/inactivityexpiration/InactivityExpirationGroup.java
+++ advancedregionmarket/src/main/java/net/alex9849/arm/inactivityexpiration/InactivityExpirationGroup.java
@@ -19,6 +19,35 @@ public class InactivityExpirationGroup {
     private long resetAfterMs;
     private long takeOverAfterMs;
 
+    private static abstract class GetBestGroupThread extends Thread {
+        private OfflinePlayer oPlayer;
+        private World world;
+        InactivityExpirationGroup result = null;
+
+        GetBestGroupThread(OfflinePlayer offlinePlayer, World world) {
+            this.oPlayer = offlinePlayer;
+            this.world = world;
+        }
+
+        void setResult(InactivityExpirationGroup ieGroup) {
+            this.result = ieGroup;
+        }
+
+        OfflinePlayer getOfflinePlayer() {
+            return this.oPlayer;
+        }
+
+        World getWorld() {
+            return this.world;
+        }
+
+        public abstract void run();
+
+        public InactivityExpirationGroup getResult() {
+            return result;
+        }
+    }
+
     public InactivityExpirationGroup(String name, long resetAfterMs, long takeOverAfterMs) {
         this.name = name;
         this.resetAfterMs = resetAfterMs;
@@ -54,47 +83,81 @@ public class InactivityExpirationGroup {
     }
 
     public static InactivityExpirationGroup getBestResetAfterMs(OfflinePlayer oPlayer, World world) {
-        if(!AdvancedRegionMarket.getInstance().getVaultPerms().isEnabled()) {
-            return UNLIMITED;
-        }
-        if(oPlayer == null || oPlayer.getName() == null) {
-            return DEFAULT;
-        }
-        if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + "unlimited")) {
-            return UNLIMITED;
-        }
-        InactivityExpirationGroup selectedGroup = DEFAULT;
-
-        for(InactivityExpirationGroup ieGroup : inactivityExpirationGroupSet) {
-            if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + ieGroup.getName())) {
-                if(selectedGroup == DEFAULT || ((!selectedGroup.isResetDisabled() && (selectedGroup.getResetAfterMs() < ieGroup.getResetAfterMs())) || ieGroup.isResetDisabled())) {
-                    selectedGroup = ieGroup;
+        GetBestGroupThread getBestGroupThread = new GetBestGroupThread(oPlayer, world) {
+            @Override
+            public void run() {
+                if(!AdvancedRegionMarket.getInstance().getVaultPerms().isEnabled()) {
+                    setResult(UNLIMITED);
+                    return;
                 }
+                if(oPlayer == null || oPlayer.getName() == null) {
+                    setResult(DEFAULT);
+                    return;
+                }
+                if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + "unlimited")) {
+                    setResult(UNLIMITED);
+                    return;
+                }
+                InactivityExpirationGroup selectedGroup = DEFAULT;
+
+                for(InactivityExpirationGroup ieGroup : inactivityExpirationGroupSet) {
+                    if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + ieGroup.getName())) {
+                        if(selectedGroup == DEFAULT || ((!selectedGroup.isResetDisabled() && (selectedGroup.getResetAfterMs() < ieGroup.getResetAfterMs())) || ieGroup.isResetDisabled())) {
+                            selectedGroup = ieGroup;
+                        }
+                    }
+                }
+                setResult(selectedGroup);
+                return;
             }
+        };
+        getBestGroupThread.start();
+        try {
+            getBestGroupThread.join();
+            return getBestGroupThread.getResult();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return UNLIMITED;
         }
-        return selectedGroup;
     }
 
     public static InactivityExpirationGroup getBestTakeOverAfterMs(OfflinePlayer oPlayer, World world) {
-        if(!AdvancedRegionMarket.getInstance().getVaultPerms().isEnabled()) {
-            return UNLIMITED;
-        }
-        if(oPlayer == null || oPlayer.getName() == null) {
-            return DEFAULT;
-        }
-        if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + "unlimited")) {
-            return UNLIMITED;
-        }
-        InactivityExpirationGroup selectedGroup = DEFAULT;
-
-        for(InactivityExpirationGroup ieGroup : inactivityExpirationGroupSet) {
-            if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + ieGroup.getName())) {
-                if(selectedGroup == DEFAULT || ((!selectedGroup.isTakeOverDisabled() && (selectedGroup.getTakeOverAfterMs() < ieGroup.getTakeOverAfterMs())) || ieGroup.isTakeOverDisabled())) {
-                    selectedGroup = ieGroup;
+        GetBestGroupThread getBestGroupThread = new GetBestGroupThread(oPlayer, world) {
+            @Override
+            public void run() {
+                if(!AdvancedRegionMarket.getInstance().getVaultPerms().isEnabled()) {
+                    setResult(UNLIMITED);
+                    return;
                 }
+                if(oPlayer == null || oPlayer.getName() == null) {
+                    setResult(DEFAULT);
+                    return;
+                }
+                if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + "unlimited")) {
+                    setResult(UNLIMITED);
+                    return;
+                }
+                InactivityExpirationGroup selectedGroup = DEFAULT;
+
+                for(InactivityExpirationGroup ieGroup : inactivityExpirationGroupSet) {
+                    if(AdvancedRegionMarket.getInstance().getVaultPerms().playerHas(world.getName(), oPlayer, Permission.ARM_INACTIVITY_EXPIRATION + ieGroup.getName())) {
+                        if(selectedGroup == DEFAULT || ((!selectedGroup.isTakeOverDisabled() && (selectedGroup.getTakeOverAfterMs() < ieGroup.getTakeOverAfterMs())) || ieGroup.isTakeOverDisabled())) {
+                            selectedGroup = ieGroup;
+                        }
+                    }
+                }
+                setResult(selectedGroup);
+                return;
             }
+        };
+        getBestGroupThread.start();
+        try {
+            getBestGroupThread.join();
+            return getBestGroupThread.getResult();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return UNLIMITED;
         }
-        return selectedGroup;
     }
 
     public static InactivityExpirationGroup parse(ConfigurationSection configurationSection, String name) {

051013728b1b958b2ad967070286dc357979b979
alex9849 <alexander@liggesmeyer.de>
2020-02-26 15:56:06 +0100

    better automatic versioning

--- .travis/mvn-deploy.sh
+++ .travis/mvn-deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "Ensuring that pom  matches $TRAVIS_TAG"
-mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
+echo "Ensuring that pom matches $TRAVIS_TAG"
+mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DartifactId=advancedregionmarket -DnewVersion=$TRAVIS_TAG
 
 echo "Uploading to oss repo and GitHub"
 mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease

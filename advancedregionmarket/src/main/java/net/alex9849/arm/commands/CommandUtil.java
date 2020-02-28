53f77d866cade833cce96aabdf32f68f383faa0b
alex9849 <alexander@liggesmeyer.de>
2020-02-28 11:45:57 +0100

    Added more commands to new command system

--- advancedregionmarket/src/main/java/net/alex9849/arm/commands/CommandUtil.java
+++ advancedregionmarket/src/main/java/net/alex9849/arm/commands/CommandUtil.java
@@ -1,5 +1,26 @@
 package net.alex9849.arm.commands;
 
+import java.util.List;
+
 public class CommandUtil {
 
+    public static <X> String getStringList(List<X> xList, StringGetter<X> stringGetter) {
+        StringBuilder sb = new StringBuilder();
+
+        boolean first = true;
+        for(X x : xList) {
+            if(first) {
+                first = false;
+            } else {
+                sb.append(", ");
+            }
+            sb.append(stringGetter.get(x));
+        }
+        return sb.toString();
+    }
+
+
+    public interface StringGetter<X> {
+        String get(X x);
+    }
 }
--- advancedregionmarket/src/main/java/net/alex9849/arm/commands/ListRegionsCommand.java
+++ advancedregionmarket/src/main/java/net/alex9849/arm/commands/ListRegionsCommand.java
@@ -3,14 +3,12 @@ package net.alex9849.arm.commands;
 import net.alex9849.arm.AdvancedRegionMarket;
 import net.alex9849.arm.Messages;
 import net.alex9849.arm.Permission;
-import net.alex9849.arm.exceptions.CmdSyntaxException;
 import net.alex9849.arm.exceptions.InputException;
 import net.alex9849.arm.handler.CommandHandler;
 import net.alex9849.arm.regions.Region;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -18,102 +16,49 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ListRegionsCommand implements BasicArmCommand {
-    private final String rootCommand = "listregions";
-    private final String regex = "(?i)listregions";
-    private final List<String> usage = new ArrayList<>(Arrays.asList("listregions", "listregions [PLAYER]"));
+public class ListRegionsCommand extends BasicArmCommand {
     private final String regex_with_args = "(?i)listregions [^;\n ]+";
 
-    @Override
-    public boolean matchesRegex(String command) {
-        return command.matches(this.regex) || command.matches(this.regex_with_args);
+    public ListRegionsCommand() {
+        super(true, "listregions",
+                Arrays.asList("(?i)listregions", "(?i)listregions [^;\n ]+"),
+                Arrays.asList("listregions", "listregions [PLAYER]"),
+                Arrays.asList(Permission.MEMBER_LISTREGIONS, Permission.ADMIN_LISTREGIONS));
     }
 
     @Override
-    public String getRootCommand() {
-        return this.rootCommand;
-    }
-
-    @Override
-    public List<String> getUsage() {
-        return this.usage;
-    }
-
-    @Override
-    public boolean runCommand(CommandSender sender, Command cmd, String commandsLabel, String[] args, String allargs) throws InputException, CmdSyntaxException {
-
-        if (allargs.matches(this.regex)) {
-            if (sender.hasPermission(Permission.MEMBER_LISTREGIONS) || sender.hasPermission(Permission.ADMIN_LISTREGIONS)) {
-                if (!(sender instanceof Player)) {
-                    throw new InputException(sender, Messages.COMMAND_ONLY_INGAME);
-                }
-                Player player = (Player) sender;
-                listRegionsCommand(sender, player.getName());
-                return true;
-            } else {
+    protected boolean runCommandLogic(CommandSender sender, String command) throws InputException {
+        String playerName = sender.getName();
+        if (command.matches(this.regex_with_args)) {
+            String[] args = command.split(" ");
+            if(!sender.getName().equalsIgnoreCase(args[1]) && sender.hasPermission(Permission.ADMIN_LISTREGIONS)) {
                 throw new InputException(sender, Messages.NO_PERMISSION);
             }
-        } else if (allargs.matches(this.regex_with_args)) {
-            if (sender.hasPermission(Permission.ADMIN_LISTREGIONS)) {
-                listRegionsCommand(sender, args[1]);
-            } else {
-                throw new InputException(sender, Messages.NO_PERMISSION);
-            }
+            playerName = args[1];
         }
 
-        return true;
-    }
-
-    private void listRegionsCommand(CommandSender sender, String playerName) throws InputException {
         OfflinePlayer oplayer = Bukkit.getOfflinePlayer(playerName);
         if (oplayer == null) {
             throw new InputException(sender, Messages.PLAYER_NOT_FOUND);
         }
 
-        List<Region> regionsOwner = AdvancedRegionMarket.getInstance().getRegionManager().getRegionsByOwner(oplayer.getUniqueId());
-        List<Region> regionsMember = AdvancedRegionMarket.getInstance().getRegionManager().getRegionsByMember(oplayer.getUniqueId());
+        List<Region> regionsOwner = AdvancedRegionMarket.getInstance()
+                .getRegionManager().getRegionsByOwner(oplayer.getUniqueId());
+        List<Region> regionsMember = AdvancedRegionMarket.getInstance()
+                .getRegionManager().getRegionsByMember(oplayer.getUniqueId());
 
-        String regionstring = "";
-        if (regionsOwner.size() > 0) {
-            regionstring = regionsOwner.get(0).getRegion().getId();
-        }
-        for (int i = 1; i < regionsOwner.size(); i++) {
-            regionstring += ", " + regionsOwner.get(i).getRegion().getId();
-        }
-
-        sender.sendMessage(ChatColor.GOLD + "Owner: " + regionstring);
-
-        regionstring = "";
-        if (regionsMember.size() > 0) {
-            regionstring = regionsMember.get(0).getRegion().getId();
-        }
-        for (int i = 1; i < regionsMember.size(); i++) {
-            regionstring += ", " + regionsMember.get(i).getRegion().getId();
-        }
-
-        sender.sendMessage(ChatColor.GOLD + "Member: " + regionstring);
-
-
+        sender.sendMessage(ChatColor.GOLD + "Owner: " + CommandUtil
+                .getStringList(regionsOwner, x -> x.getRegion().getId()));
+        sender.sendMessage(ChatColor.GOLD + "Member: " + CommandUtil
+                .getStringList(regionsMember, x -> x.getRegion().getId()));
+        return true;
     }
 
     @Override
-    public List<String> onTabComplete(Player player, String[] args) {
-        List<String> returnme = new ArrayList<>();
-
-        if (args.length >= 1) {
-            if (this.rootCommand.startsWith(args[0])) {
-                if (player.hasPermission(Permission.ADMIN_LISTREGIONS) || player.hasPermission(Permission.MEMBER_LISTREGIONS)) {
-                    if (args.length == 1) {
-                        returnme.add(this.rootCommand);
-                    } else if (args.length == 2 && (args[0].equalsIgnoreCase(this.rootCommand))) {
-                        if (player.hasPermission(Permission.ADMIN_LISTREGIONS)) {
-                            returnme.addAll(CommandHandler.tabCompleteOnlinePlayers(args[1]));
-                        }
-                    }
-                }
-            }
+    protected List<String> onTabCompleteLogic(Player player, String[] args) {
+        if (player.hasPermission(Permission.ADMIN_LISTREGIONS)) {
+            return CommandHandler.tabCompleteOnlinePlayers(args[1]);
         }
-        return returnme;
-
+        return new ArrayList<>();
     }
 }

dd3180caf435eda6587faa5c650d3f00bde7a5b6
alex9849 <alexander@liggesmeyer.de>
2020-03-15 10:37:15 +0100

    Automatic code cleanup by ItelliJ

--- wg6_2adapter/src/main/java/net/alex9849/adapters/WorldGuard6_2.java
+++ wg6_2adapter/src/main/java/net/alex9849/adapters/WorldGuard6_2.java
@@ -1,6 +1,7 @@
 package net.alex9849.adapters;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.*;
 
 public class WorldGuard6_2 extends WorldGuard6_1 {
 
